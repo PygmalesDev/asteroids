0ec1811ad9d9104f889f76f12e90fd751053271b
PygmalesDev <uk084094@student.uni-kassel.de>
2024-09-25 08:35:29 +0200

    (feat): added player object; player movement and rotation logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -9,14 +10,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    plr = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        plr.update(dt)
+        plr.draw(screen)
 
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
